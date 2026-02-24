dc2c3fa6733b378d8cda0bdeff15303ecc4df0e5
Kwan-Daegun <160981535+Kwan-Daegun@users.noreply.github.com>
2026-02-24 16:19:03 +0800

    finished

--- Assets/_Tanks/Scripts/UI/Hover.cs
+++ Assets/_Tanks/Scripts/UI/Hover.cs
@@ -1,35 +1,22 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Hover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class Hover : MonoBehaviour
 {
-    public float offset = 10f;
-    public float speed = 10f;
+    public float amplitude = 10f;   
+    public float frequency = 1f;    
 
     RectTransform rt;
     Vector2 startPos;
-    Vector2 targetPos;
 
     void Awake()
     {
         rt = GetComponent<RectTransform>();
         startPos = rt.anchoredPosition;
-        targetPos = startPos;
     }
 
     void Update()
     {
-        rt.anchoredPosition =
-            Vector2.Lerp(rt.anchoredPosition, targetPos, Time.deltaTime * speed);
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        targetPos = startPos + Vector2.up * offset;
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        targetPos = startPos;
+        float y = Mathf.Sin(Time.unscaledTime * frequency * Mathf.PI * 2f) * amplitude;
+        rt.anchoredPosition = startPos + Vector2.up * y;
     }
 }
